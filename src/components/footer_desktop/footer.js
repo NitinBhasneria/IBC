a6d1976bbf6659bc3f3d30a665d0dbc3e7796362
NitinBhasneria <nitinbhasneria360@gmail.com>
2020-11-09 21:22:15 +0530

    Pixel1280

--- src/components/footer_desktop/footer.js
+++ src/components/footer_desktop/footer.js
@@ -15,15 +15,15 @@ function  Footer() {
                     </Row>
                     <Row className="w-100 mr-5 pt-xl-5 pt-lg-4 pt-sm-3 ml-0 pr-2"><div className="w-100 hr_footer"></div></Row>
                     <Row>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="who_are_we pt-lg-4 pt-md-3 pt-sm-3">WE ARE THE FUTURE</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">WHO ARE WE</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">LEADERSHIP TEAM</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">INVESTOR RELATION</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">THE THIRD SPACE</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">CARRERS</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">MEDIA CENTER</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">SUSTAINABILITY</Col>
-                        <Col lg={{ span: 2, offset: 10 }} md={{ span: 3, offset: 9 }} sm={{ span: 4, offset: 8 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">CONTACT US</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="who_are_we pt-lg-4 pt-md-3 pt-sm-3">WE ARE THE FUTURE</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">WHO ARE WE</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">LEADERSHIP TEAM</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">INVESTOR RELATION</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">THE THIRD SPACE</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">CARRERS</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">MEDIA CENTER</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">SUSTAINABILITY</Col>
+                        <Col lg={{ span: 3, offset: 9 }} md={{ span: 3, offset: 9 }} sm={{ span: 3, offset: 9 }} className="info_content_18 pt-lg-4 pt-md-3 pt-sm-3">CONTACT US</Col>
                     </Row>
                 </Container>
             </Container>
